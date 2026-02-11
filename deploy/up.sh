91029152a63183aec67747a5f97cb850baa081e4
Paul Mikol <paul.mikol@gmail.com>
2026-02-10 19:51:23 -0600

    auto: sync from droplet

--- deploy/up.sh
+++ deploy/up.sh
@@ -16,7 +16,7 @@ echo "[up] Generating env + secrets"
 bash -x "$DEPLOY_DIR/scripts/init-env.sh"
 
 echo "[up] Building + starting stack"
-docker compose -p "$COMPOSE_PROJECT_NAME" -f "$DEPLOY_DIR"/compose.yml build --no-cache
+#docker compose -p "$COMPOSE_PROJECT_NAME" -f "$DEPLOY_DIR"/compose.yml build --no-cache
 
 echo "[up] Building + starting stack"
 docker compose -p "$COMPOSE_PROJECT_NAME" -f "$DEPLOY_DIR"/compose.yml up -d --force-recreate --renew-anon-volumes --remove-orphans
