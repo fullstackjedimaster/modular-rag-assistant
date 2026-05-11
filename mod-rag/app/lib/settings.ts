6cfc6b687bbd489f1b86beffe9a2aae9c39c84e5
Paul Mikol <paul.mikol@gmail.com>
2026-05-11 15:14:11 -0500

    auto: sync from droplet

--- mod-rag/app/lib/settings.ts
+++ mod-rag/app/lib/settings.ts
@@ -15,7 +15,7 @@ export const settings = {
 
   AI_CORE_BASE:
      process.env.NEXT_PUBLIC_AI_CORE_BASE ||
-     "https://ai-core.fullstackjedi.dev",
+     "http://localhost:8001",
 
   DATABASE_URL:
    process.env.DATABASE_URL ||
