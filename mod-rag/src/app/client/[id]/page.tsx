7f7b78aef677aa0cb194ee3601a2f8253af2d85c
Paul Mikol <paul.mikol@gmail.com>
2026-06-05 03:26:42 -0500

    auto: sync from droplet

--- mod-rag/src/app/client/[id]/page.tsx
+++ mod-rag/src/app/client/[id]/page.tsx
@@ -3,4 +3,14 @@ import ManagementShell from "@/src/components/management/ManagementShell";
 
 
 
+type PageProps = {
+    params: Promise<{
+        id: string;
+    }>;
+};
+
+export default async function ClientPage({ params }: PageProps) {
+    const { id } = await params;
+    return <ManagementShell mode="edit" clientId={id} />;
+}
 
